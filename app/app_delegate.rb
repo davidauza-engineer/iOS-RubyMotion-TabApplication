02a6bff59971009c5ef2ca32f5d01a9c4a82515c
davidauza.engineer <davidauza01@gmail.com>
2021-01-12 09:08:18 -0500

    Update app_delegate.rb

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -13,19 +13,14 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
     red_view_controller = RedViewController.alloc.initWithNibName(nil, bundle: nil)
-
     blue_view_controller = BlueViewController.alloc.initWithNibName(nil, bundle: nil)
-
     white_view_controller = WhiteViewController.alloc.initWithNibName(nil, bundle: nil)
 
     tab_bar_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-
     tab_bar_controller.viewControllers = [red_view_controller, blue_view_controller, white_view_controller]
-
     @window.rootViewController = tab_bar_controller
 
     @window.makeKeyAndVisible
-
     true
   end
 end
